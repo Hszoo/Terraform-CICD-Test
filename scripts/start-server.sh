cbbb847966c99da2b8292e3d422e5affdb2b322e
Hszoo <trre1827151@gmail.com>
2025-10-24 07:44:52 +0900

    [Fix] cicd scripts

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "--------------- START : AWS-CICD-Pipeline Server Deploy -----------------"
-cd /home/ec2-user/AWS-CICD-Pipeline
+echo "--------------- START : Terraform-CICD-Pipeline Server Deploy -----------------"
+cd /home/ec2-user/hb05-terraform
 
 echo "[Check] 현재 디렉토리: $(pwd)"
 echo "[Check] 파일 목록:"
 ls -al
 
 echo "[AWS] ECR 로그인"
-aws ecr get-login-password --region ap-northeast-2 \
-  | docker login --username AWS --password-stdin 329863774288.dkr.ecr.ap-northeast-2.amazonaws.com/hybrid05-server-registry
+ECR_REGISTRY="329863774288.dkr.ecr.us-east-2.amazonaws.com"
+aws ecr get-login-password --region us-east-2 \
+  | docker login --username AWS --password-stdin "$ECR_REGISTRY"
   
 echo "[Docker Compose] 실행 중인 컨테이너 중지"
 docker compose down || true
@@ -18,4 +19,4 @@ docker compose down || true
 echo "[Docker Compose] 컨테이너 실행"
 docker compose up -d
 
-echo "-------------- COMPLETE : AWS-CICD-Pipeline Server Deploy ---------------"
+echo "-------------- COMPLETE : Terraform-CICD-Pipeline Server Deploy ---------------"
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 컨테이너 stop & rm
-if [ "$(docker ps -q -f name=hybrid05-server)" ]; then
-    docker stop hybrid05-server
-    docker rm hybrid05-server
+if [ "$(docker ps -q -f name=hybrid05-web)" ]; then
+    docker stop hybrid05-web
+    docker rm hybrid05-web
 else
-    echo "Container hybrid05-server does not exist, skipping stop."
+    echo "Container hybrid05-web does not exist, skipping stop."
 fi
 
 # ECR 로그인
-ECR_REGISTRY="329863774288.dkr.ecr.ap-northeast-2.amazonaws.com"
-aws ecr get-login-password --region ap-northeast-2 \
+ECR_REGISTRY="329863774288.dkr.ecr.us-east-2.amazonaws.com"
+aws ecr get-login-password --region us-east-2 \
   | docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
 # ECR에서 최신 이미지 pull
-ECR_IMAGE="$ECR_REGISTRY/hybrid05-server-registry:latest"
+ECR_IMAGE="$ECR_REGISTRY/hb05-ecr:latest"
 docker pull "$ECR_IMAGE"
